4f354f025c8b4ae3351c808f20acc1e334aa90b9
Hemanth Haluvadi Machegowda <99693832+hemanthhm29@users.noreply.github.com>
2023-04-11 19:48:35 -0400

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+export class AppComponent {
+  showHeader: boolean = false;
+
+  constructor(private router: Router) {
+    router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        if (val.url == '/login' || val.url == '/register') {
+          this.showHeader = false;
+        } else {
+          this.showHeader = true;
+        }
+      }
+    });
+  }
+}
